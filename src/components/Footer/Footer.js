ed4fa9d35ddab30feaf6d79bd8785f97bd9020ad
Tom Riley <tom.riley@outlook.com>
2022-05-11 16:38:24 +0100

    added footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,13 +2,48 @@ import React from 'react';
 import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
 
 import { SocialIcons } from '../Header/HeaderStyles';
-import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTitle, Slogan, SocialContainer, SocialIconsContainer } from './FooterStyles';
+import {
+  CompanyContainer,
+  FooterWrapper,
+  LinkColumn,
+  LinkItem,
+  LinkList,
+  LinkTitle,
+  Slogan,
+  SocialContainer,
+  SocialIconsContainer,
+} from './FooterStyles';
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <FooterWrapper>
+      <LinkList>
+        <LinkColumn>
+          <LinkTitle>☎️</LinkTitle>
+          <LinkItem href='tel:+44(0)7468605902'>+44 (0) 7468605902</LinkItem>
+        </LinkColumn>
+        <LinkColumn>
+          <LinkTitle>📧</LinkTitle>
+          <LinkItem href='mailto:hello@tomriley.dev'>
+            hello@tomriley.dev
+          </LinkItem>
+        </LinkColumn>
+      </LinkList>
+      <SocialIconsContainer>
+        <CompanyContainer>
+          <Slogan>Innovating forever baby</Slogan>
+        </CompanyContainer>
+        <SocialIcons href='https://github.com/TomCRiley'>
+          <AiFillGithub size='3rem' />
+        </SocialIcons>
+        <SocialIcons href='https://github.com/TomCRiley'>
+          <AiFillLinkedin size='3rem' />
+        </SocialIcons>
+        <SocialIcons href='https://github.com/TomCRiley'>
+          <AiFillInstagram size='3rem' />
+        </SocialIcons>
+      </SocialIconsContainer>
+    </FooterWrapper>
   );
 };
 
