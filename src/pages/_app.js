5add043db1ec99b4dc6b1e6ea72b8e953b98d2fe
Tom Riley <tom.riley@outlook.com>
2022-05-20 11:58:11 +0100

    updated favicon

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,6 +4,11 @@ import { Head } from 'next/document';
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <Head>
+          <link rel='shortcut icon' href='/favicon.ico' />
+        </Head>
+      </Head>
       <Theme>
         <Component {...pageProps} />
       </Theme>
